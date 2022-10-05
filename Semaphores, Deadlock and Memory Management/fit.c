0096753a4f538a33f491b4a1620b37d5aa920115
Noel Martin <noelmartin0@gmail.com>
2022-10-05 13:32:41 +0530

    final

--- Semaphores, Deadlock and Memory Management/fit.c
+++ Semaphores, Deadlock and Memory Management/fit.c
@@ -1,148 +1,113 @@
 #include<stdio.h>
-struct block{
-int no;
-int size;
-int flag;
-}b[10];
-struct process{
-int no;
-int size;
-int allocblock;
+struct block
+{
+   int no;
+   int size;
+   int flag;
+}b[10],temp;
+struct process
+{
+   int no;
+   int size;
+   int allocblock;
 }p[10];
-int bn,pn,temp;
+int pn,bn;
+void display(int pn) //To Display the content
+{
+   printf("Pno\tPsize\tBno\tBsize\tWastage\n");
+   for(int i=1;i<=pn;i++)
+   {
+      if(p[i].allocblock!=0)
+         printf("%d\t%d\t%d\t%d\t%d\n",p[i].no,p[i].size,p[i].allocblock,b[p[i].allocblock].size,b[p[i].allocblock].size - p[i].size);
+      else
+         printf("%d\t%d\tUnallocated\t\t\n",p[i].no,p[i].size);
+   }
+}
+void check() //To allocate the process
+{
+   for(int i=1;i<=pn;i++)
+   {  
+      for (int j=1;j<=bn;j++)
+      {
+         if(b[j].flag !=1 && p[i].size<=b[j].size)
+         {
+            b[j].flag=1;
+            p[i].allocblock = j;
+         }
+         if(p[i].allocblock!=0)
+            break;
+      }
+   }
+}
+void reset() //To clear the values to 0
+{
+    for(int i=1;i<=bn;i++)
+      b[i].flag=0;
+   for(int j=1;j<=pn;j++)
+      p[j].allocblock=0;
+}
+void main()
+{
+   printf("\nEnter the no. of blocks : ");
+   scanf("%d",&bn);
+   for(int i=1;i<=bn;i++)
+   {
+      printf("\nEnter the Block size : ");
+      scanf("%d",&b[i].size);
+      b[i].no = i;
+      b[i].flag=0;
+   }
+   printf("\nEnter the no. of processes : ");
+   scanf("%d",&pn);
+   for(int i=1;i<=pn;i++)
+   {
+      printf("\nEnter the Process size : ");
+      scanf("%d",&p[i].size);
+      p[i].no = i;
+      p[i].allocblock =0;
+   }
 
-void main(){
-printf("\nEnter the no. of blocks : ");
-scanf("%d",&bn);
+   //FIRST FIT
 
-for(int i=1;i<=bn;i++){
-printf("\nEnter the Block size : ");
-scanf("%d",&b[i].size);
-   b[i].no = i;
-   b[i].flag=0;
-}
+   check();
+   printf("\nFirst Fit\n");
+   display(pn);
 
-printf("\nEnter the no. of processes : ");
-scanf("%d",&pn);
+   //BEST FIT
 
-for(int i=1;i<=pn;i++){
-printf("\nEnter the Process size : ");
-scanf("%d",&p[i].size);
-p[i].no = i;
-p[i].allocblock =0;
-}
+  reset();
+   for(int i=1;i<=bn;i++)
+   {
+      for(int j=1;j<bn-i+1;j++)
+      {
+         if(b[j+1].size < b[j].size)
+         {
+            temp = b[j];
+            b[j] = b[j+1];
+            b[j+1] = temp;
+         }
+      }
+   }
+   check();
+   printf("\nBest fit\n");
+   display(pn);
 
+   //WORST FIT
 
-for(int i=1;i<=pn;i++){
-for (int j=1;j<=bn;j++){
-if(b[j].flag !=1 && p[i].size<=b[j].size){
-b[j].flag=1;
-p[i].allocblock = b[j].no;
-}
-if(p[i].allocblock!=0){
-break;
-}
-}
-
-}
-printf("\nFirst Fit\n");
-printf("Pno\tPsize\tBno\tBsize\tWastage\n");
-for(int i=1;i<=pn;i++){
-if(p[i].allocblock!=0){
-printf("%d\t%d\t%d\t%d\t%d\n",p[i].no,p[i].size,p[i].allocblock,b[p[i].allocblock].size,b[p[i].allocblock].size - p[i].size);
-}
-else{
-printf("%d\t%d\tUnallocated\t\t\n",p[i].no,p[i].size);
-}
-}
-//best fit
-
-for(int i=1;i<=bn;i++){
-b[i].flag=0;
-}
-for(int j=1;j<=pn;j++){
-p[j].allocblock=0;
-}
-
-for(int i=1;i<=bn;i++){
-for(int j=1;j<bn-i+1;j++){
-if(b[j+1].size < b[j].size){
-temp = b[j].size;
-b[j].size = b[j+1].size;
-b[j+1].size = temp;
-
-temp = b[j].no;
-b[j].no = b[j+1].no;
-b[j+1].no = temp;
-}
-}
-}
-
-
-for(int i=1;i<=pn;i++){
-for (int j=1;j<=bn;j++){
-if(b[j].flag !=1 && p[i].size<=b[j].size){
-b[j].flag=1;
-p[i].allocblock = j;
-}
-if(p[i].allocblock!=0){
-break;
-}
-}
-
-}
-printf("\nBest fit\n");
-printf("Pno\tPsize\tBno\tBsize\tWastage\n");
-for(int i=1;i<=pn;i++){
-if(p[i].allocblock!=0){
-printf("%d\t%d\t%d\t%d\t%d\n",p[i].no,p[i].size,p[i].allocblock,b[p[i].allocblock].size,b[p[i].allocblock].size - p[i].size);
-}
-else{
-printf("%d\t%d\tUnallocated\t\t\n",p[i].no,p[i].size);
-}
-}
-
-for(int i=1;i<=bn;i++){
-b[i].flag=0;
-}
-for(int j=1;j<=pn;j++){
-p[j].allocblock=0;
-}
-
-for(int i=1;i<=bn;i++){
-for(int j=1;j<bn-i+1;j++){
-if(b[j+1].size > b[j].size){
-temp = b[j].size;
-b[j].size = b[j+1].size;
-b[j+1].size = temp;
-
-temp = b[j].no;
-b[j].no = b[j+1].no;
-b[j+1].no = temp;
-}
-}
-}
-
-for(int i=1;i<=pn;i++){
-for (int j=1;j<=bn;j++){
-if(b[j].flag !=1 && p[i].size<=b[j].size){
-b[j].flag=1;
-p[i].allocblock = j;
-}
-if(p[i].allocblock!=0){
-break;
-}
-}
-
-}
-printf("\nWorst fit\n");
-printf("Pno\tPsize\tBno\tBsize\tWastage\n");
-for(int i=1;i<=pn;i++){
-if(p[i].allocblock!=0){
-printf("%d\t%d\t%d\t%d\t%d\n",p[i].no,p[i].size,p[i].allocblock,b[p[i].allocblock].size,b[p[i].allocblock].size - p[i].size);
-}
-else{
-printf("%d\t%d\tUnallocated\t\t\n",p[i].no,p[i].size);
-}
-}
+   reset();
+   for(int i=1;i<=bn;i++)
+   {
+      for(int j=1;j<bn-i+1;j++)
+      {
+         if(b[j+1].size > b[j].size)
+         {
+            temp = b[j];
+            b[j] = b[j+1];
+            b[j+1] = temp;
+         }
+      }
+   }
+   check();
+   printf("\nWorst fit\n");
+   display(pn);
 }
